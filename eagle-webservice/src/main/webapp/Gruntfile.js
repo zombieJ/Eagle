c3020eb2f9221a4b6f33b2c04640ffae7f0e4371
jiljiang <jiljiang@ebay.com>
2015-11-24 04:10:56 +0000

    update package

--- eagle-webservice/src/main/webapp/Gruntfile.js
+++ eagle-webservice/src/main/webapp/Gruntfile.js
@@ -29,18 +29,63 @@ module.exports = function (grunt) {
 			dist: ['dist/'],
 		},
 		concat: {
-			//app: {
-			//	src: [
-			//		'tmp/js/templates.js',
-			//		'app/js/**/*.js'
-			//	],
-			//	dest: 'tmp/js/scripts.js'
-			//},
+			app: {
+				src: [
+					"public/js/app.ui.js",
+					"public/js/components/main.js",
+					"public/js/components/sortTable.js",
+					"public/js/components/tabs.js",
+					"public/js/components/file.js",
+					"public/js/components/charts.js",
+					"public/js/components/charts/bar.js",
+					"public/js/components/charts/line3d.js",
+
+					"public/js/common.js",
+					"public/js/config.js",
+
+					"public/js/app.time.js",
+					"public/js/app.js",
+					"public/js/ctrl/damController.js",
+					"public/js/ctrl/policyController.js",
+					"public/js/ctrl/siteController.js",
+					"public/js/ctrl/streamController.js",
+					"public/js/ctrl/alertController.js",
+					"public/js/ctrl/sensitivityController.js",
+					"public/js/ctrl/userProfileController.js",
+					"public/js/ctrl/authController.js",
+				],
+				dest: 'tmp/public/js/scripts.js'
+			},
 			js: '<%= config.concat.js %>',
-			css: '<%= config.concat.css %>'
+			css: {
+				options: {
+					process: function(src, filepath) {
+						return "@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic);" +
+						src.replace('@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic);', '');
+					}
+				},
+				src: '<%= config.concat.css.src %>',
+				dest: '<%= config.concat.css.dest %>',
+			}
+		},
+		'regex-replace': {
+			strict: {
+				src: ['tmp/public/js/scripts.js'],
+				actions: [
+					{
+						name: 'use strict',
+						search: '\\\'use strict\\\';?',
+						replace: '',
+						flags: 'gmi'
+					}
+				]
+			},
 		},
 		uglify: {
 			dist: {
+				options: {
+					mangle: false
+				},
 				src: 'tmp/public/js/scripts.js',
 				dest: 'tmp/public/js/scripts.min.js'
 			}
@@ -48,7 +93,7 @@ module.exports = function (grunt) {
 		cssmin: {
 			dist: {
 				files: {
-					'tmp/public/css/styles.min.css': ['tmp/public/css/styles.css']
+					'tmp/public/css/styles.css': ['public/css/main.css']
 				}
 			}
 		},
@@ -64,6 +109,7 @@ module.exports = function (grunt) {
 					{expand: true, cwd: 'tmp/', src: ['**'], dest: 'dist'},
 					{expand: true, src: ['public/images/**', 'partials/**'], dest: 'dist'},
 					{expand: true, cwd: 'node_modules/font-awesome/', src: ['fonts/**'], dest: 'dist/public'},
+					{expand: true, cwd: 'node_modules/bootstrap/', src: ['fonts/**'], dest: 'dist/public'},
 				]
 			}
 		}
@@ -74,9 +120,24 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-htmlrefs');
+	grunt.loadNpmTasks('grunt-regex-replace');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	//grunt.registerTask('default', ['clean:build','concat', 'uglify', 'cssmin', 'htmlrefs', 'copy', 'clean:tmp']);
-	//grunt.registerTask('default', ['clean:build','concat', 'uglify', 'cssmin', 'htmlrefs', 'copy']);
-	grunt.registerTask('default', ['clean:dist', 'copy']);
+	grunt.registerTask('default', [
+		// Clean Env
+		'clean:build',
+		// Compress JS
+		'concat:app',
+		'regex-replace:strict',
+		'uglify',
+		'concat:js',
+		// Compress CSS
+		'cssmin',
+		'concat:css',
+		// Pass HTML Resources
+		'htmlrefs',
+		'copy',
+		// Clean Env
+		'clean:tmp',
+	]);
 };
